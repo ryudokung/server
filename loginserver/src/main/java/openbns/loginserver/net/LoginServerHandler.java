2d8fb18fd968dee8a15df67fcdbc9a6485f5f531
Eugene Chipachenko <echipachenko@gmail.com>
2014-01-17 22:32:46 +0200

    Base routes

--- loginserver/src/main/java/openbns/loginserver/net/LoginServerHandler.java
+++ loginserver/src/main/java/openbns/loginserver/net/LoginServerHandler.java
@@ -2,13 +2,12 @@ package openbns.loginserver.net;
 
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.ChannelInboundHandlerAdapter;
-import openbns.commons.net.codec.sts.DefaultStsResponse;
 import openbns.commons.net.codec.sts.DefaultStsRequest;
+import openbns.commons.net.codec.sts.DefaultStsResponse;
 import openbns.commons.net.codec.sts.StsResponseStatus;
 import openbns.commons.net.codec.sts.StsVersion;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
-
 
 /**
  * Created with IntelliJ IDEA.
@@ -27,33 +26,21 @@ public class LoginServerHandler extends ChannelInboundHandlerAdapter
     {
       DefaultStsRequest req = (DefaultStsRequest) msg;
 
-
       String method = req.getMethod().name();
       switch( method )
       {
         case "/Sts/Connect":
           System.out.println( "Client request conenct" );
-          DefaultStsResponse response = new DefaultStsResponse( StsVersion.STS_1_0, StsResponseStatus.OK);
+          DefaultStsResponse response = new DefaultStsResponse( StsVersion.STS_1_0, StsResponseStatus.OK );
+          ctx.write( response );
+          break;
+        case "/Sts/Ping":
+          // TODO:
+          break;
+        case "/Auth/LoginStart":
+          // TODO:
           break;
       }
-//      if( is100ContinueExpected( req ) )
-//      {
-//        ctx.write( new DefaultFullStsResponse( HTTP_1_1, CONTINUE ) );
-//      }
-//      boolean keepAlive = isKeepAlive( req );
-//      FullHttpResponse response = new DefaultFullStsResponse( HTTP_1_1, OK, Unpooled.wrappedBuffer( "Hello world".getBytes() ) );
-//      response.headers().set( CONTENT_TYPE, "text/plain" );
-//      response.headers().set( CONTENT_LENGTH, response.content().readableBytes() );
-//
-//      if( !keepAlive )
-//      {
-//        ctx.write( response ).addListener( ChannelFutureListener.CLOSE );
-//      }
-//      else
-//      {
-//        response.headers().set( CONNECTION, Values.KEEP_ALIVE );
-//        ctx.write( response );
-//      }
     }
   }
 
