5abae99053c07148af8b71bec988163796e2dd83
Eugene Chipachenko <echipachenko@gmail.com>
2014-01-25 12:01:52 +0200

    JDBC pooling

---
+++ loginserver/src/main/java/openbns/DataBaseFactory.java
@@ -0,0 +1,45 @@
+package openbns;
+
+import org.apache.commons.dbcp.BasicDataSourceFactory;
+
+import javax.sql.DataSource;
+import java.io.InputStream;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.Properties;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Eugene Chipachenko
+ * Date: 25.01.14
+ * Time: 11:49
+ */
+public class DataBaseFactory
+{
+  private static DataBaseFactory ourInstance = new DataBaseFactory();
+  private DataSource dataSource;
+
+  public static DataBaseFactory getInstance()
+  {
+    return ourInstance;
+  }
+
+  private DataBaseFactory()
+  {
+    try (InputStream is = DataBaseFactory.class.getResourceAsStream( "/DataSource.properties" ))
+    {
+      Properties properties = new Properties();
+      properties.load( is );
+      dataSource = BasicDataSourceFactory.createDataSource( properties );
+    }
+    catch( Exception e )
+    {
+      System.exit( 1 );
+    }
+  }
+
+  public Connection getConnection() throws SQLException
+  {
+    return dataSource.getConnection();
+  }
+}
