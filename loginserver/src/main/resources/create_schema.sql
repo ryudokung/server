73d003139940b078068d183f6e2528c5c9f411a0
Eugene Chipachenko <echipachenko@gmail.com>
2014-02-01 10:59:01 +0200

    add database model for MySQL workbench

--- loginserver/src/main/resources/create_schema.sql
+++ loginserver/src/main/resources/create_schema.sql
@@ -1,1 +1,67 @@
-CREATE TABLE `accounts` (  `objectId`     VARCHAR(36) NOT NULL,  `login`        VARCHAR(16) NOT NULL,  `password`     BLOB(32)    NOT NULL,  `accessLevel`  TINYINT     NULL,  `lastLogin`    DATETIME    NULL,  `lastIp`       VARCHAR(15) NULL,  `lastServerId` INT         NULL DEFAULT 0,  PRIMARY KEY (`objectId`, `login`));CREATE TABLE `accounts_log` (  `id`        INT         NOT NULL AUTO_INCREMENT,  `login`     VARCHAR(16) NOT NULL,  `operation` TINYINT     NULL,  `msgId`     SMALLINT    NULL,  `time`      DATETIME    NULL,  PRIMARY KEY (`id`));CREATE TABLE `accounts_settings` (  `id`    INT         NOT NULL AUTO_INCREMENT,  `login` VARCHAR(16) NOT NULL,  `key`   VARCHAR(64) NULL,  `value` VARCHAR(64) NULL,  PRIMARY KEY (`id`, `login`, `key`));ALTER TABLE `accounts_log` ADD FOREIGN KEY (`login`) REFERENCES `accounts` (`login`);ALTER TABLE `accounts_settings` ADD FOREIGN KEY (`login`) REFERENCES `accounts` (`login`);
+SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
+SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
+SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL,ALLOW_INVALID_DATES';
+
+DROP SCHEMA IF EXISTS `openbns` ;
+CREATE SCHEMA IF NOT EXISTS `openbns` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci ;
+USE `openbns` ;
+
+-- -----------------------------------------------------
+-- Table `openbns`.`accounts`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `openbns`.`accounts` ;
+
+CREATE TABLE IF NOT EXISTS `openbns`.`accounts` (
+  `uuid` VARCHAR(36) NOT NULL,
+  `login` VARCHAR(16) NOT NULL,
+  `password` BLOB(32) NOT NULL,
+  `accessLevel` INT NOT NULL DEFAULT 0,
+  `lastLogin` DATETIME NULL,
+  `lastIp` VARCHAR(15) NULL,
+  `lastServerId` TINYINT NULL DEFAULT 0,
+  PRIMARY KEY (`uuid`),
+  UNIQUE INDEX `login_UNIQUE` (`login` ASC))
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `openbns`.`accounts_log`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `openbns`.`accounts_log` ;
+
+CREATE TABLE IF NOT EXISTS `openbns`.`accounts_log` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `login` VARCHAR(16) NOT NULL,
+  `msgId` SMALLINT NOT NULL,
+  `time` DATETIME NOT NULL,
+  PRIMARY KEY (`id`),
+  CONSTRAINT `fk_login`
+    FOREIGN KEY (`login`)
+    REFERENCES `openbns`.`accounts` (`login`)
+    ON DELETE CASCADE
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `openbns`.`accounts_info`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `openbns`.`accounts_info` ;
+
+CREATE TABLE IF NOT EXISTS `openbns`.`accounts_info` (
+  `login` VARCHAR(16) NOT NULL,
+  `birthday` DATETIME NULL,
+  `question` VARCHAR(255) NULL,
+  `answer` VARCHAR(255) NULL,
+  PRIMARY KEY (`login`),
+  CONSTRAINT `fk_login`
+    FOREIGN KEY (`login`)
+    REFERENCES `openbns`.`accounts` (`login`)
+    ON DELETE CASCADE
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+SET SQL_MODE=@OLD_SQL_MODE;
+SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
+SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
